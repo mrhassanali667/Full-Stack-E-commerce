b742018d5d3496d7a697fc7d99bf6aaae26ac2f0
Hassan-Ali0 <hassanali031935@gmail.com>
2026-02-09 19:14:31 +0500

    update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Navbar = () => {
   return (
     <header className='w-full bg-[#232f3f] '>
-      <div className='w-full container py-2 flex  m-auto '>
-        <div className='w-[50%] flex text-[#e0e0e0]'>
-          <span className='text-[0.85em]  hover:text-orange-400'>Free Intenarional Shiping. No Minimum Purchase Required*</span>
+      <div className='w-full container py-2 px-5 flex justify-between  m-auto '>
+        <div className=' flex text-[#e0e0e0]'>
+          <span className='max-lg:hidden text-[0.85em]  hover:text-orange-400'>Free Intenarional Shiping. No Minimum Purchase Required*</span>
         </div>
-        <ul className='w-[50%] flex justify-end gap-6 list-none  text-[#e0e0e0] [&>li]:text-[0.85em] [&>li]:cursor-pointer [&>li]:hover:text-orange-400 [&>li]:transition-colors [&>li]:duration-300 '>
+        <ul className=' flex justify-end gap-6 list-none text-[#e0e0e0] [&>li]:text-[0.85em] [&>li]:cursor-pointer [&>li]:hover:text-orange-400 [&>li]:transition-colors [&>li]:duration-300 '>
           <li>My Acount</li>
           <li>Wish List</li>
           <li>Check out</li>
@@ -16,22 +16,22 @@ const Navbar = () => {
         </ul>
       </div>
       <div className='w-full px-10 py-5 flex gap-5 justify-between  m-auto'>
-        <div className='flex items-center gap-5 grow'>
+        <div className='flex items-center gap-10 grow'>
           <div className='flex gap-2 items-center'>
             <h1 className='font-space font-bold text-[2.5em] text-white'>Multi</h1>
             <span className='bg-white font-space rounded text-center font-semibold text-[#ff7e00] text-[0.75em] px-[4px]'>MART</span>
           </div>
-          <div className='flex max-h-[45px] w-full max-w-[500px]'>
-            <input type="search" name="" id="" className='flex-1 bg-white px-5 py-2 outline-0 text-gray-800 rounded-l-full' />
-            <button className='rounded-r-full px-5 text-white  bg-[#ff7e00]'>Search</button>
+          <div className='flex h-full  max-h-[45px] w-full max-w-[700px]'>
+            <input type="search" placeholder='Search for anything…' className='h-full flex-1 bg-white text-[0.9em] px-8 py-2 outline-0 text-gray-800 rounded-l-full' />
+            <button className='h-auto font-semibold rounded-r-full px-10 text-white  bg-[#ff7e00]'>SEARCH</button>
           </div>
         </div>
         <div className='max-w-[1000px] flex gap-8'>
           {/* User Account */}
-          <div className='flex gap-2'>
-            <div className='h-[45px] w-[45px] flex justify-center items-center rounded-full border-1 border-gray-400'>
+          <div className='flex group gap-2 hover:cursor-pointer'>
+            <div className='h-[45px] w-[45px] flex justify-center items-center rounded-full border-1 border-gray-400 group-hover:bg-[#ff7e00] transition-colors duration-300'>
               <svg
-                className="w-7 h-7 text-[#ff7e00]"
+                className="w-7 h-7 text-[#ff7e00] group-hover:text-white transition-colors duration-300"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 width={24}
@@ -51,10 +51,10 @@ const Navbar = () => {
               <span className='text-gray-400 text-[0.9em]'>User Name</span>
             </div>
           </div>
-          <div className='flex gap-2'>
-            <div className='h-[45px] w-[45px] flex justify-center items-center rounded-full border-1 border-gray-400'>
+          <div className='flex group gap-2 hover:cursor-pointer'>
+            <div className='h-[45px] w-[45px] group-hover:bg-[#ff7e00] flex justify-center items-center rounded-full border-1 border-gray-400 transition-colors duration-500'>
               <svg
-                className="w-7 h-7 text-[#ff7e00]"
+                className="w-7 h-7 text-[#ff7e00] group-hover:text-white transition-colors duration-500"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 width={24}
@@ -77,18 +77,34 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      <div className='w-full flex px-6 m-auto'>
-        <div className='text-white w-70 bg-[#ff7e00] text-[1.5em] py-2 px-10'>
-          Category
+      <div className='w-full flex px-10 m-auto'>
+        <div className='text-white w-55 flex items-center justify-between bg-[#ff7e00] font-semibold text-[1.1em] py-3 px-7'>
+          <span>CATEGORIES</span>
+          <svg
+            className="w-8 h-8 text-white self-end"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth={2}
+              d="M5 7h14M5 12h14M5 17h14"
+            />
+          </svg>
+
         </div>
         <nav className='grow flex px-4 ml-7'>
-          <ul className='flex items-center gap-7 text-white font-semibold [&>li]:text-[0.9em]'>
+          <ul className='flex items-center gap-7 text-white font-semibold [&>li]:text-[0.9em] [&>li]:cursor-pointer [&>li]:hover:text-[#ff7e00] [&>li]:transition-colors [&>li]:duration-300'>
             <li>HOME</li>
             <li>PAGES</li>
             <li>BLOGES</li>
             <li>CONTACTS</li>
             <li>BUY THEME NOW</li>
-            <li></li>
           </ul>
         </nav>
       </div>
