44bca6440a09c69ce984e2696f1ca48ec1067b44
Hassan-Ali0 <hassanali031935@gmail.com>
2026-03-24 11:34:38 +0500

    add carousel

--- src/App.jsx
+++ src/App.jsx
@@ -4,57 +4,50 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-function NextButton() {
-  const swiper = useSwiper();
 
-  return (
-    <button onClick={() => swiper.slideNext()} className=' absolute top-1/2 right-4 transform -translate-y-1/2 bg-red-400 text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500' >
-      Next
-    </button>
-  );
-}
-
-function PreviousButton() {
-  const swiper = useSwiper();
-
-  return (
-    <button onClick={() => swiper.slidePrev()} className='absolute top-1/2 left-4 transform -translate-y-1/2 bg-red-400 text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500' >
-      Previous
-
-    </button>
-  );
-}
 
 function App() {
+
+  const [swiper, setSwiper] = useState(null);
 
 
   return (
     <>
-      <div className='h-screen w-screen  font-sans'>
+      <div className='h-screen w-screen overflow-y-auto  font-sans'>
         <Navbar />
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={3  }
-          loop={true}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-        >
-          <SwiperSlide><div className='h-20  bg-gray-400 p-4'>Slide 1</div></SwiperSlide>
-          <SwiperSlide><div className='h-20  bg-pink-400 p-4'>Slide 2</div></SwiperSlide>
-          <SwiperSlide><div className='h-20  bg-blue-400 p-4'>Slide 3</div></SwiperSlide>
-          <SwiperSlide><div className='h-20  bg-green-400 p-4'>Slide 4</div></SwiperSlide>
-          <SwiperSlide><div className='h-20  bg-violet-400 p-4'>Slide 5</div></SwiperSlide>
-        </Swiper>
+        <div className=''>
+          <Swiper
+            modules={[ Pagination]}
+            pagination={{ clickable: true }}
+            // autoplay={{ delay: 3000, disableOnInteraction: false }}
+            allowTouchMove={false}  
+            speed={900}
+            spaceBetween={0}
+            slidesPerView={1}
+            loop={true}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => setSwiper(swiper)}
+          >
+            <SwiperSlide><div className='h-130  bg-[url(./carousel-1.jpg)] bg-cover bg-center bg-no-repeat' >Slide 1</div></SwiperSlide>
+            <SwiperSlide><div className='h-130  bg-[url(./carousel-2.jpg)] bg-cover bg-center bg-no-repeat' >Slide 2</div></SwiperSlide>
+            <SwiperSlide><div className='h-130  bg-[url(./carousel-3.jpg)] bg-cover bg-center bg-no-repeat' >Slide 3</div></SwiperSlide>
+            <SwiperSlide><div className='h-130  bg-[url(./carousel-4.jpg)] bg-cover bg-center bg-no-repeat' >Slide 4</div></SwiperSlide>
+          </Swiper>
+        </div>
 
-        <PreviousButton />
-        <NextButton />
+        <button onClick={() => swiper.slidePrev()} className=' m-10  bg-red-400 text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500' >
+          Previous
+        </button>
 
-
+        <button onClick={() => swiper.slideNext()} className=' bg-red-400 text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500' >
+          Next
+        </button>
       </div>
 
     </>
